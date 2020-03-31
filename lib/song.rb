d027c05a0fb4b3b75df69aad85a6a96ea8d8c8fb
Diane Crame <dcrame@icloud.com>
2020-03-31 00:38:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,9 +30,12 @@ class Song
   end
   
   def self.genre_count
-    
+    new_arr = @@genres
+    new_arr.inject(Hash.new(0)) {|genre, num| genre[num]+=1; genre}
   end
 
   def self.artist_count
+    new_arr = @@artists
+    new_arr.inject(Hash.new(0)) {|genre, num| genre[num]+=1; genre}
   end
 end
